35bf73ccc873cf1f08c89d290a080f6258445fd4
ryojo <joaovicioxd@gmail.com>
2020-04-20 18:00:47 -0300

    versão 1.0

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+
+class Home extends StatefulWidget {
+   
+  @override
+  _HomeState createState() => _HomeState();
+ 
+}
+
+class _HomeState extends State<Home> {
+ var _frases = [
+    "asdasd",  "vcvcxvxcvxv" 
+    ];
+
+    var _frasesGerada = "Clique abaixo para gerar uma frase!";
+  
+  void _gerarFrases(){
+    var sortear = Random().nextInt(_frases.length);
+  
+  setState(() {
+    _frasesGerada = _frases[sortear];
+  });
+  }
+   
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+        appBar: AppBar(
+          title: Text("Frases do dia"),
+          backgroundColor: Colors.green,
+        ),
+        body:Container(
+            padding: EdgeInsets.all(16),
+            width:double.infinity ,
+             /* decoration: BoxDecoration(
+                border: Border.all(width:3, color: Colors.amber),
+          )*/
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+         crossAxisAlignment: CrossAxisAlignment.center,
+         children: <Widget>[
+             Image.asset("images/logo.png"),
+              Text(
+          _frasesGerada,
+          textAlign: TextAlign.justify,
+          style: TextStyle(
+              fontSize: 17,
+              fontStyle: FontStyle.italic,
+              color: Colors.black
+          ),
+                ),
+                RaisedButton(child: Text(
+                  "Nova Frase",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+
+
+                  ),
+                ),
+                color: Colors.green,
+                onPressed: _gerarFrases,
+                ),
+         ],
+       ),
+        ),
+    );
+  }
+}
